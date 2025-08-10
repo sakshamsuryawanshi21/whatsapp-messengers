5809404748fcb943417d6169c657dd4a815951dd
sakshamsuryawanshi21 <sakshamsuryawansh194@gmail.com>
2025-08-11 00:02:45 +0530

    Update API base URL to production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL:"https://whatsapp-messengers-production.up.railway.app/api"});
 
 export const getChats = () => API.get("/chats");
 export const getMessages = (wa_id) => API.get(`/chats/${wa_id}`);
