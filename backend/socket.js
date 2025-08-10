23fbc1245b502052f6ab2299b178735fe9be591b
sakshamsuryawanshi21 <sakshamsuryawansh194@gmail.com>
2025-08-11 00:54:06 +0530

    Update socket.js

--- backend/socket.js
+++ backend/socket.js
@@ -3,10 +3,10 @@ let io;
 module.exports = {
   init: (server) => {
     io = require('socket.io')(server, {
-          "Access-Control-Allow-Origin": "*",
-          origin: ["https://whatsapp-messengers.vercel.app","http://localhost:5174",],
-          methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-          allowedHeaders: "Content-Type,Authorization", // Specify the allowed headers
+          
+          origin: ['https://whatsapp-messengers.vercel.app', 'http://localhost:5174'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     });
     return io;
   },
