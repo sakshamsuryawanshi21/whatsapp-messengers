e9f37af319000695898ea463886a15e75656b805
sakshamsuryawanshi21 <sakshamsuryawansh194@gmail.com>
2025-08-11 01:17:53 +0530

    Your message

--- backend/socket.js
+++ backend/socket.js
@@ -3,9 +3,13 @@ let io;
 module.exports = {
   init: (server) => {
     io = require('socket.io')(server, {
-      cors: { origin: 'http://localhost:5173' },
-    });
-    return io;
+  cors: { 
+    origin: ['http://localhost:5173', 'https://whatsapp-messengers.vercel.app'],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
   },
   getIO: () => {
     if (!io) {
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL:"https://whatsapp-messengers-production.up.railway.app/api"});
+const API = axios.create({ baseURL: "https://whatsapp-messengers-production.up.railway.app/api" });
 
 export const getChats = () => API.get("/chats");
 export const getMessages = (wa_id) => API.get(`/chats/${wa_id}`);
